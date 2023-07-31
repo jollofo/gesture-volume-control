fd89ac83fb0c6a1c3642d4dc03330643047c45af
James Muguiyi <jmuguiyijr@gmail.com>
2023-07-31 12:20:03 -0400

    Updated VolumeHandControl

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
-from ctypes import cast, pointer
+from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
@@ -14,7 +14,7 @@ cap.set(3, wCam)
 cap.set(4, hcam)
 pTime = 0
 
-detector = htm.handDetector(detectionCon=0.7)
+detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
 device = AudioUtilities.GetSpeakers()
 interface = device.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
@@ -28,31 +28,32 @@ volPer = 0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
+    lmList, bbox = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        
+        if 250 < area < 1000:
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+            volBar = np.interp(length, [50, 200], [400, 150])
+            volPer = np.interp(length, [50, 200], [0, 100])
+            smoothness = 10
+            volPer = smoothness * round(volPer / smoothness)
+            fingers = detector.fingersUp()
 
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED) 
-        cv2.line(img, (x1, x2), (x2, y2), (255, 0, 255), 3) 
-        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
-        length = math.hypot(x2 - x1, y2 - y1)
-
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volBar = np.interp(length, [50, 300], [400, 150])
-        volPer = np.interp(length, [50, 300], [0, 100])
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
-
-        if length < 50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+            else:
+                colorVol = (255, 0, 0)
+    
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
     cv2.putText(img, f"{int(volPer)} %", (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
-
+    
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
+    
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
