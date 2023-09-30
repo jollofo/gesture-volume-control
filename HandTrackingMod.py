2f9dcaace63245e9b0a603fa5bdd91a56a6167c1
James Muguiyi <jmuguiyijr@gmail.com>
2023-09-30 12:26:24 -0400

    Fixed dependency bug

--- HandTrackingMod.py
+++ HandTrackingMod.py
@@ -20,7 +20,6 @@ class handDetector():
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -36,12 +35,10 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
                 yList.append(cy)
-                # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
@@ -57,12 +54,10 @@ class handDetector():
 
     def fingersUp(self):
         fingers = []
-        # Thumb
         if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
-        # 4 Fingers
         for id in range(1, 5):
             if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
                 fingers.append(1)
--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-import HandTrackingModule as htm
+import HandTrackingMod as htm
 import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
@@ -25,7 +25,7 @@ maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
-colorVol = (255, 0, 0)
+colorVol = (0, 255, 0)
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -46,11 +46,11 @@ while True:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 colorVol = (0, 255, 0)
             else:
-                colorVol = (255, 0, 0)
+                colorVol = (0, 255, 0)
     
-    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
-    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f"{int(volPer)} %", (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 0, 255), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 0, 255), cv2.FILLED)
+    cv2.putText(img, f"{int(volPer)} %", (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
     
     cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
     cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
@@ -58,7 +58,7 @@ while True:
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
     cv2.imshow("Img", img)
     cv2.waitKey(1)
 
