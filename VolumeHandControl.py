21bb2a0d69b83f89b93ecafb99590b76a5fd0b28
James Muguiyi <jmuguiyijr@gmail.com>
2023-09-30 12:34:56 -0400

    Fixed dependency bug

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -56,9 +56,7 @@ while True:
     cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
 
     cTime = time.time()
-    fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
     cv2.imshow("Img", img)
     cv2.waitKey(1)
 
